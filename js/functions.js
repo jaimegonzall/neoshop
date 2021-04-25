1f1747a14aa35123ae9b0f8eabadf7db0e2c3e94
jaimegonzall <jaimegonzall@gmail.com>
2021-04-25 13:49:36 +0200

    Entrega Slider

--- js/functions.js
+++ js/functions.js
@@ -71,3 +71,44 @@ function brilloPrecio() {
 productsArticle.forEach(boton => {
     boton.addEventListener("mouseover", brilloPrecio)
 });
+
+
+
+// SLIDE
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+  muestraSlides(indice+=n);
+}
+
+function posicionSlide(n){
+  muestraSlides(indice=n);
+}
+
+setInterval(function tiempo(){
+  muestraSlides(indice+=1)
+},3000);
+
+function muestraSlides(n){
+  let i;
+  let slides = document.getElementsByClassName('slider__slide');
+  let puntos = document.getElementsByClassName('puntos__dot');
+
+  if(n > slides.length){
+    indice = 1;
+  }
+  if(n < 1){
+    indice = slides.length();
+  }
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = 'none'
+  }
+  for(i = 0; i < puntos.length; i++){
+    puntos[i].className = puntos[i].className.replace(" active", "");
+  }
+
+  slides[indice-1].style.display = 'block';
+  puntos[indice-1].className += ' active';
+
+}
